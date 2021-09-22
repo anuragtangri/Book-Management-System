6b72df843dd0958fb5f1205c3407bffdc6252270
Anurag Tangri <anuragtangri321@gmail.com>
2021-09-22 02:43:12 -0400

    updates

--- go-bookstore/pkg/utils/utils.go
+++ go-bookstore/pkg/utils/utils.go
@@ -1,18 +1,18 @@
-// package utils
+package utils
 
-// import (
-// 	"encoding/json"
-// 	"fmt"
-// 	"io/ioutil"
-// 	"net/http"
-// )
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
 
-// func ParseBody(r *http.Request, x interface{}) {
-// 	body, _ := ioutil.ReadAll(r.Body)
-// 	fmt.Println(string(body))
-// 	err := json.Unmarshal(body, &x)
-// 	fmt.Println(x)
-// 	if err == nil {
-// 		return
-// 	}
-// }
+func ParseBody(r *http.Request, x interface{}) {
+	body, _ := ioutil.ReadAll(r.Body)
+	fmt.Println(string(body))
+	err := json.Unmarshal(body, &x)
+	fmt.Println(x)
+	if err == nil {
+		return
+	}
+}
